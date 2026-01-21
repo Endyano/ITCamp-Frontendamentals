43eb4fce3799941cdc90f7e1774ffac97ccc4811
Endy <endyanoang@gmail.com>
2026-01-21 21:58:27 +0800

    saving

--- curriculum/phase2-javascript/05-js-advance/starter/exercise1.js
+++ curriculum/phase2-javascript/05-js-advance/starter/exercise1.js
@@ -1,10 +1,17 @@
 const user = { //name and active state 
+    name: "Endy",
+    isActive: true
 };
+
 
 // Before:
 function getCard(user) {
+    if(user.isActive){
+        return `User ${user} is Active`
+    } else
+        return `User ${user} is offline`
 
 }
 
 // After (they need to write this):
-const getCard = 
+const getCard = (user) => `User ${user.name} is ${user.isActive ? "Active" : "Offline"}`;
